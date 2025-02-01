b5a144f369f8038e8befaeef3daebe625009b49a
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-02-01 14:49:03 +0530

    01-02-25

---
+++ OrangeHRM/src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,71 @@
+package stepDefinitions;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.cucumber.java.en.*;
+
+public class LoginStepDefinition {
+	
+	WebDriver driver;
+	
+	@Given("chrome browser is launched")
+	public void chrome_browser_is_launched() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		driver = new ChromeDriver(options);
+	    
+	}
+	
+	@Given("user navigated to OrangeHRM Login page")
+	public void user_navigated_to_orange_hrm_login_page() {
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+	
+	@When("user enters Username")
+	public void user_enters_username() {
+		WebElement username_txtbx = driver.findElement(By.name("username"));
+		username_txtbx.sendKeys("Admin");
+	}
+	
+	@When("user enters Password")
+	public void user_enters_password() {
+		WebElement password_txtbx = driver.findElement(By.name("password"));
+		password_txtbx.sendKeys("admin123");
+	}
+	
+	@When("user clicks on Login button")
+	public void user_clicks_on_login_button() {
+		WebElement login_btn = driver.findElement(By.tagName("button"));
+		login_btn.click();
+	}
+	
+	@Then("user should able to see Dashboard URL")
+	public void user_should_able_to_see_dashboard_url() {
+		String expected_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+		String actual_url = driver.getCurrentUrl();
+		assertEquals("Login failed", expected_url, actual_url);
+		
+	}
+	
+	@When("user enters Username {string}")
+	public void user_enters_username(String username) {
+		WebElement username_txtbx = driver.findElement(By.name("username"));
+		username_txtbx.sendKeys(username);
+	}
+	
+	@When("user enters Password {string}")
+	public void user_enters_password(String password) {
+		WebElement password_txtbx = driver.findElement(By.name("password"));
+		password_txtbx.sendKeys(password);
+	}
+
+}
